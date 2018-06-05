ebe3de8d3c2056b04756ce325ddde5565fbed8c7
sena <luka.senicic@gmail.com>
2018-06-05 17:25:43 +0200

    Fetch2

--- src/index.tsx
+++ src/index.tsx
@@ -68,7 +68,7 @@ class Index extends React.Component<{}, AppState> {
                     source={this.state.gifSource}
                 />
 
-                {'Just some randoooom giphy'}
+                {'Just some randoooom giphy!!!'}
                 <form
                     onSubmit={this.onSubmit}
                 >
